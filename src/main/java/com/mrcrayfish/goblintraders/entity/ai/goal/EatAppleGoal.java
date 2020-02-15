c6b01b72f3bec72b50a095baca652787075ac907
MrCrayfish <mrcrayfish@hotmail.com>
2020-02-15 20:37:54 +1030

    Reduced chance that goblin trader will eat

--- src/main/java/com/mrcrayfish/goblintraders/entity/ai/goal/EatAppleGoal.java
+++ src/main/java/com/mrcrayfish/goblintraders/entity/ai/goal/EatAppleGoal.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.ai.goal.UseItemGoal;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.SoundEvents;
 
 import java.util.EnumSet;
 
@@ -15,7 +16,7 @@ public class EatAppleGoal extends UseItemGoal<AbstractGoblinEntity>
 {
     public EatAppleGoal(AbstractGoblinEntity entity)
     {
-        super(entity, new ItemStack(Items.APPLE), null, entity1 -> entity1.getHealth() < entity1.getMaxHealth() && entity1.getRNG().nextInt(40) == 0); //TODO change sound to a burp
+        super(entity, new ItemStack(Items.APPLE), SoundEvents.ENTITY_PLAYER_BURP, entity1 -> entity1.getHealth() < entity1.getMaxHealth() && entity1.getRNG().nextInt(100) == 0); //TODO change sound to a burp
         this.setMutexFlags(EnumSet.of(Goal.Flag.MOVE));
     }
 }
