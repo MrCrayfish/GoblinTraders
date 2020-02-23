a1316f9e88a4dc0efa92776c1c47f35696dcd1d1
MrCrayfish <mrcrayfish@hotmail.com>
2020-02-23 23:10:54 +1030

    Woops

--- src/main/java/com/mrcrayfish/goblintraders/init/ModEntities.java
+++ src/main/java/com/mrcrayfish/goblintraders/init/ModEntities.java
@@ -2,6 +2,7 @@ package com.mrcrayfish.goblintraders.init;
 
 import com.mrcrayfish.goblintraders.Reference;
 import com.mrcrayfish.goblintraders.entity.GoblinTraderEntity;
+import com.mrcrayfish.goblintraders.entity.VeinGoblinTraderEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
@@ -25,6 +26,7 @@ public class ModEntities
     private static final List<EntityType<?>> ENTITY_TYPES = new ArrayList<>();
 
     public static final EntityType<GoblinTraderEntity> GOBLIN_TRADER = build(new ResourceLocation(Reference.MOD_ID, "goblin_trader"), GoblinTraderEntity::new, 0.5F, 1.0F);
+    public static final EntityType<VeinGoblinTraderEntity> VEIN_GOBLIN_TRADER = build(new ResourceLocation(Reference.MOD_ID, "vein_goblin_trader"), VeinGoblinTraderEntity::new, 0.5F, 1.0F);
 
     private static <T extends Entity> EntityType<T> build(ResourceLocation id, Function<World, T> function, float width, float height)
     {
