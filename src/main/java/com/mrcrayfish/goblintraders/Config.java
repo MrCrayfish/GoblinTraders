e5802d4d576890dd18c08619e2e1b99997b6dceb
MrCrayfish <mrcrayfish@hotmail.com>
2021-09-13 18:35:40 +0930

    📦 Overworld goblins should no longer spawn on the surface

--- src/main/java/com/mrcrayfish/goblintraders/Config.java
+++ src/main/java/com/mrcrayfish/goblintraders/Config.java
@@ -17,7 +17,7 @@ public class Config
         Common(ForgeConfigSpec.Builder builder)
         {
             builder.comment("Common configuration settings").push("common");
-            this.goblinTrader = new Goblin(builder, "Goblin Trader", "goblin_trader", 25, 24000, 0, 64);
+            this.goblinTrader = new Goblin(builder, "Goblin Trader", "goblin_trader", 25, 24000, 0, 63);
             this.veinGoblinTrader = new Goblin(builder, "Vein Goblin Trader", "vein_goblin_trader", 25, 24000, 0, 128);
             this.preventDespawnIfNamed = builder.comment("If true, prevents the trader from despawning if named").define("preventDespawnIfNamed", true);
             builder.pop();
