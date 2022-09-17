53a92ef5e4230f46237d3a85da59f3cdfae3af67
MrCrayfish <mrcrayfish@hotmail.com>
2022-09-18 05:53:53 +0930

    ✨ Added config options to change number of trades

--- src/main/java/com/mrcrayfish/goblintraders/Config.java
+++ src/main/java/com/mrcrayfish/goblintraders/Config.java
@@ -1,9 +1,12 @@
 package com.mrcrayfish.goblintraders;
 
 import com.mrcrayfish.configured.api.simple.BoolProperty;
+import com.mrcrayfish.configured.api.simple.DoubleProperty;
 import com.mrcrayfish.configured.api.simple.IntProperty;
 import com.mrcrayfish.configured.api.simple.SimpleConfig;
 import com.mrcrayfish.configured.api.simple.SimpleProperty;
+import com.mrcrayfish.goblintraders.trades.IRaritySettings;
+import com.mrcrayfish.goblintraders.trades.TradeRarity;
 
 /**
  * Author: MrCrayfish
@@ -54,6 +57,9 @@ public final class Config
             @SimpleProperty(name = "gruntNoiseInterval", comment = "Goblins will make a grunt noise while walking around. If you find it happening too often, you can increase the interval. Value is represented in ticks.")
             public final IntProperty gruntNoiseInterval;
 
+            @SimpleProperty(name = "trades")
+            public final Trades trades = new Trades();
+
             private Goblin(int spawnChance, int spawnDelay, int minLevel, int maxLevel)
             {
                 this.traderSpawnChance = IntProperty.create(spawnChance, 1, 100);
@@ -63,6 +69,73 @@ public final class Config
                 this.restockDelay = IntProperty.create(48000, -1, Integer.MAX_VALUE);
                 this.canAttackBack = BoolProperty.create(true);
                 this.gruntNoiseInterval = IntProperty.create(80, 1, 1000);
+            }
+
+            public static class Trades
+            {
+                @SimpleProperty(name = "common")
+                public final Trade common = new Trade(5, 8, 1.0);
+
+                @SimpleProperty(name = "uncommon")
+                public final Trade uncommon = new Trade(3, 5, 1.0);
+
+                @SimpleProperty(name = "rare")
+                public final Trade rare = new Trade(3, 4, 1.0);
+
+                @SimpleProperty(name = "epic")
+                public final Trade epic = new Trade(0, 2, 1.0);
+
+                @SimpleProperty(name = "legendary")
+                public final Trade legendary = new Trade(1, 1, 0.1);
+
+                public IRaritySettings getSettings(TradeRarity rarity)
+                {
+                    return switch(rarity)
+                    {
+                        case COMMON -> this.common;
+                        case UNCOMMON -> this.uncommon;
+                        case RARE -> this.rare;
+                        case EPIC -> this.epic;
+                        case LEGENDARY -> this.legendary;
+                    };
+                }
+
+                public static class Trade implements IRaritySettings
+                {
+                    @SimpleProperty(name = "minAmount", comment = "The minimum amount of trades that a golbin will have.")
+                    public final IntProperty minAmount;
+
+                    @SimpleProperty(name = "manAmount", comment = "The maximum amount of trades that a golbin can have.")
+                    public final IntProperty maxAmount;
+
+                    @SimpleProperty(name = "includeChance", comment = "The chance this trade rarity will be included in the goblin's trades")
+                    public final DoubleProperty includeChance;
+
+                    public Trade(int min, int max, double includeChance)
+                    {
+                        this.minAmount = IntProperty.create(min, 0, Integer.MAX_VALUE);
+                        this.maxAmount = IntProperty.create(max, 0, Integer.MAX_VALUE);
+                        this.includeChance = DoubleProperty.create(includeChance, 0.0, 1.0);
+                    }
+
+                    @Override
+                    public int getMinValue()
+                    {
+                        return this.minAmount.get();
+                    }
+
+                    @Override
+                    public int getMaxValue()
+                    {
+                        return this.maxAmount.get();
+                    }
+
+                    @Override
+                    public double includeChance()
+                    {
+                        return this.includeChance.get();
+                    }
+                }
             }
         }
     }
---
+++ src/main/java/com/mrcrayfish/goblintraders/trades/IRaritySettings.java
@@ -0,0 +1,13 @@
+package com.mrcrayfish.goblintraders.trades;
+
+/**
+ * Author: MrCrayfish
+ */
+public interface IRaritySettings
+{
+    int getMinValue();
+
+    int getMaxValue();
+
+    double includeChance();
+}
