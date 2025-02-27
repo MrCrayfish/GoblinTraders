ed75e5ccfe4c47d4ff86aa0b86b219a89bc37341
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-02-28 06:43:50 +1030

    🐛 Fix goblins trying to walk to unreachable block pos

--- common/src/main/java/com/mrcrayfish/goblintraders/entity/ai/goal/FindFavouriteFoodGoal.java
+++ common/src/main/java/com/mrcrayfish/goblintraders/entity/ai/goal/FindFavouriteFoodGoal.java
@@ -7,7 +7,6 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.level.pathfinder.Path;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Comparator;
 import java.util.EnumSet;
@@ -19,7 +18,7 @@ import java.util.List;
 public class FindFavouriteFoodGoal extends Goal
 {
     private ItemEntity itemEntity;
-    private AbstractGoblinEntity entity;
+    private final AbstractGoblinEntity entity;
 
     public FindFavouriteFoodGoal(AbstractGoblinEntity entity)
     {
@@ -31,7 +30,15 @@ public class FindFavouriteFoodGoal extends Goal
     public boolean canUse()
     {
         this.findFavouriteFood();
-        return this.itemEntity != null && this.itemEntity.isAlive() && this.entity.getNavigation().createPath(this.itemEntity, 0) != null && !this.entity.isStunned();
+
+        if(this.itemEntity == null || !this.itemEntity.isAlive())
+            return false;
+
+        Path path = this.entity.getNavigation().createPath(this.itemEntity, 0);
+        if(path == null || !path.canReach())
+            return false;
+
+        return !this.entity.isStunned();
     }
 
     @Override
@@ -57,11 +64,10 @@ public class FindFavouriteFoodGoal extends Goal
         return this.itemEntity.isAlive() && this.entity.getNavigation().createPath(this.itemEntity, 0) != null;
     }
 
-    @Nullable
     private void findFavouriteFood()
     {
         List<ItemEntity> players = this.entity.level().getEntitiesOfClass(ItemEntity.class, this.entity.getBoundingBox().inflate(10), itemEntity -> itemEntity.getItem().getItem() == this.entity.getFavouriteFood().getItem());
-        if(players.size() > 0)
+        if(!players.isEmpty())
         {
             this.itemEntity = players.stream().min(Comparator.comparing(this.entity::distanceTo)).get();
         }
