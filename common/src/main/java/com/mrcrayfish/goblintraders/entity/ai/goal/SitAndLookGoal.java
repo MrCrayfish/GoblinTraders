11575e51ca294d768406593db959b59f540e4c73
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-03-01 21:31:48 +1030

    📦 Reduce frequency of sit goal

--- common/src/main/java/com/mrcrayfish/goblintraders/entity/ai/goal/SitAndLookGoal.java
+++ common/src/main/java/com/mrcrayfish/goblintraders/entity/ai/goal/SitAndLookGoal.java
@@ -34,7 +34,7 @@ public class SitAndLookGoal extends Goal
         if(this.goblin.isSitting())
             return false;
 
-        return this.goblin.getRandom().nextInt(120) == 0;
+        return this.goblin.getRandom().nextInt(200) == 0;
     }
 
     @Override
