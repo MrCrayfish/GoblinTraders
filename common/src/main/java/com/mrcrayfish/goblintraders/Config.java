d05947c4681bca7d9f06f4640d07749d3857b272
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-03-03 13:31:16 +1030

    📦 Limit legendary to 1 but increase chance

--- common/src/main/java/com/mrcrayfish/goblintraders/Config.java
+++ common/src/main/java/com/mrcrayfish/goblintraders/Config.java
@@ -145,7 +145,7 @@ public final class Config
                 public final Trade epic = new Trade(1, 2, 1.0);
 
                 @ConfigProperty(name = "legendary")
-                public final Trade legendary = new Trade(1, 2, 0.4);
+                public final Trade legendary = new Trade(1, 1, 0.75);
 
                 public IRaritySettings getSettings(TradeRarity rarity)
                 {
